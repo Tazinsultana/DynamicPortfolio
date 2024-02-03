a82971988ac78025cb0f5e1633c9b06ce31ed5e8
Tazin Sultana <tazimsultana457@gmail.com>
2024-02-03 12:01:39 +0600

    adminpenal added'

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.layouts.main')
+@section('page_title')
+    E-Need || Dashboard
+@endsection
+@section('content')
+    Hi Ami Dashboard
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BackendController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/homepage',[FrontendController::class,'Index']);
+
+Route::get('/admin/dashboard',[BackendController::class,'Index']);
